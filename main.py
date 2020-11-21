fdc0851cf4980005b744b2a7f51ce58e5c3087c8
chritoto <32043457+chritoto@users.noreply.github.com>
2020-11-21 13:37:40 -0500

    Using background

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import numpy as np
 
 from utilities import *
 
+#Background
+bg = None 
+
 cv2.namedWindow("Camera Feed")
 
 camera = cv2.VideoCapture(0)
@@ -14,22 +17,39 @@ if camera.isOpened():  # try to get the first frame
 else:
     rval = False
 
+num_frames = 0
 
 while rval:
     rval, frame = camera.read()
 
     frame, roi, roi_gray = make_frame_roi(frame)
+    
+    if num_frames < 30:
+            bg = average(roi_gray, bg, 0.5)
+    else:
+    
+        hand = identifyhand(roi_gray, bg, 10)
 
-    edges = cv2.Canny(roi_gray, 100, 200)
+        if hand is not None:
+            
+            # draw the segmented region and display the frame
+            cv2.drawContours(frame, hand, -1, (0, 0, 255))
 
-    frame_cnts = get_contours(edges, frame)
-
+    # Draw rectangle on image
+    cv2.rectangle(frame, (top, left), (bottom, right), (255, 0, 0), 1)
+    
+    #update number of frames
+    num_frames += 1
+    
     # Draw contours over the ROI
-    cv2.imshow("Camera Feed", frame_cnts)
+    cv2.imshow("Camera Feed", frame)
 
     key = cv2.waitKey(20)
     if key == 27:  # exit on ESC
         break
+    if key == 114: #Reload background on r
+        bg = None
+        num_frames = 0
 
-cv2.destroyWindow("Camera Feed")
-vc.release()
+cv2.destroyAllWindows()
+camera.release()
--- utilities.py
+++ utilities.py
@@ -4,7 +4,6 @@ import logging
 
 # Globals
 background = None
-num_frames = 0
 top, left, bottom, right = 0, 0, 300, 300
 
 
@@ -24,33 +23,64 @@ def make_frame_roi(frame):
     # Copy the frame so we can alter it as we want without having to deal with the rectangle
     clone = frame.copy()
 
-    # Draw rectangle on frame
-    frame = cv2.rectangle(frame, (top, left), (bottom, right), (255, 0, 0), 1)
-
     # Define ROI
     roi = clone[top:bottom, left:right]
 
     # Gray-scale ROI
     roi_gray = cv2.cvtColor(roi.copy(), cv2.COLOR_BGR2GRAY)
+    
+    #Blur to keep low frequencies
+    roi_gray = cv2.GaussianBlur(roi_gray, (7, 7), 0)
 
     return frame, roi, roi_gray
 
 
-def get_contours(edges, frame):
-    """Locate contours of the edges
+def identifyhand(image, bg, threshold):
+    """Locate the contours of the hand
 
     Args:
-        edges (array-like): Edge detection image (Canny edge detection result)
+        image (array-like): image of interest grayscaled
+        bg (array-like): Background of image
+        threshold (float): threshold for countours
 
     Returns:
-        array-like: The contours in the ROI
+        array-like : The largest contour in the ROI
     """
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9, 9))
-    dilated = cv2.dilate(edges, kernel)
+    diff = cv2.absdiff(bg.astype("uint8"), image)
 
-    cnts, _ = cv2.findContours(
-        dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
-    )
-    frame_cnts = cv2.drawContours(frame, cnts, -1, (0, 255, 0), 3)
+    # threshold the diff image so that we get the foreground
+    thresholded = cv2.threshold(diff, threshold, 255, cv2.THRESH_BINARY)[1]
 
-    return frame_cnts
+    # get the contours in the thresholded image
+    (cnts, _) = cv2.findContours(thresholded.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # return None, if no contours detected
+    if len(cnts) == 0:
+        return None
+    else:
+        # based on contour area, get the maximum contour which is the hand
+        hand = max(cnts, key=cv2.contourArea)
+        return hand
+
+def average(image, bg, weight):
+    """Adds image to the average of the background with the weight specified
+
+    Args:
+        image (array-like): Image to add to average
+        bg (array-like): current background
+        weight: weight of new image in background
+        
+    Returns:
+        (array-like): the resulting bg
+
+    """
+    
+    # initialize the background
+    if bg is None:
+        bg = image.copy().astype("float")
+        return bg
+
+    # compute weighted average, accumulate it and update the background
+    cv2.accumulateWeighted(image, bg, weight)
+    
+    return bg
